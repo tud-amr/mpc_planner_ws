4afc6731494da4b0dde3decb30d33b4a20fab86b
oscardegroot <groot.oscar.de@gmail.com>
2024-06-06 13:00:08 +0200

    add connect to jackal script.

---
+++ connect_to_jackal.sh
@@ -0,0 +1,4 @@
+source devel/setup.bash
+alias connect_to_jackal3="export ROS_MASTER_URI=http://cpr-j100-0594:11311;export ROS_IP=192.168.0.25"
+alias connect_to_jackal1="export ROS_MASTER_URI=http://cpr-j100-0114:11311;export ROS_IP=192.168.0.128"
+connect_to_jackal3
