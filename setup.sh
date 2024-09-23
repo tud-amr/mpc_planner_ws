2c9c5fdd74c8a24133d2912a4e1c90fa6402f54c
oscardegroot <groot.oscar.de@gmail.com>
2024-09-23 09:05:39 +0200

    Separate lab.repos, add acados installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -e
 
 # Import repositories from vcs file
 vcs import < planner.repos src --recursive 
+# vcs import < lab.repos src --recursive # Uncomment to clone lab packages
 
 # Switch to ROS1 mode
 cd src
@@ -23,6 +24,28 @@ cd pedestrian_simulator
 python3 switch_to_ros.py 1
 cd ..
 cd ..
+
+# Install Acados
+git clone https://github.com/acados/acados.git
+cd acados
+git submodule update --recursive --init
+mkdir -p build
+cd build
+cmake -DACADOS_SILENT=ON ..
+make install -j4
+make shared_library
+
+if command -v poetry &> /dev/null
+then
+    echo "Poetry is installed."
+else
+    cd src/mpc_planner
+    poetry lock
+    poetry install --no-interaction --no-root
+    poetry add -e /acados/interfaces/acados_template
+    cd ../..
+    echo "Done."
+fi
 
 # Install dependencies
 sudo apt-get update -y
