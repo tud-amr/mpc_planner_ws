8e9a837be4cd5d8ffc8243c4e865cb6efcbfd1c9
oscardegroot <groot.oscar.de@gmail.com>
2024-09-23 09:22:36 +0200

    Remove nvidia runtime.

--- build.sh
+++ build.sh
@@ -1,7 +1,30 @@
+#!/bin/bash
+# Arguments: system_type generate_solver
 clear
 
 . /opt/ros/noetic/setup.sh
 . workspace/devel/setup.sh
+
+if [ -z "$2" ]; then
+  echo "Not rebuilding the solver."
+else
+  # Check if the argument equals "True" or "true"
+  if [ "$2" = "True" ] || [ "$2" = "true" ]; then
+    cd src/mpc_planner
+    python3 -m poetry run python mpc_planner_$1/scripts/generate_$1_solver.py
+
+    if  [ ${PIPESTATUS[0]} -eq 0 ]
+    then
+      echo "Solver built successfully."
+    else
+      echo "Failed to build the solver."
+      exit 0
+    fi
+
+  else
+    echo "Not building the solver."
+  fi
+fi
 
 # BUILD_TYPE=Debug # Release, Debug, RelWithDebInfo, MinSizeRel
 BUILD_TYPE=RelWithDebInfo # Release, Debug, RelWithDebInfo, MinSizeRel
