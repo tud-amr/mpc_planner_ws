bc7ee122a9b380f1d744339f3f50f0e29b814477
oscardegroot <groot.oscar.de@gmail.com>
2024-12-11 10:07:32 +0100

    minor updates to integrate jackal better.

--- connect_to_jackal.sh
+++ connect_to_jackal.sh
@@ -1,4 +1,10 @@
 source devel/setup.bash
-alias connect_to_jackal3="export ROS_MASTER_URI=http://cpr-j100-0594:11311;export ROS_IP=192.168.0.25"
-alias connect_to_jackal1="export ROS_MASTER_URI=http://cpr-j100-0114:11311;export ROS_IP=192.168.0.128"
-connect_to_jackal3
+
+# Jackal 1
+# export ROS_MASTER_URI=http://cpr-j100-0114:11311
+
+# Jackal 3
+export ROS_MASTER_URI=http://cpr-j100-0594:11311
+
+# Your IP (check with `ip a`)
+export ROS_IP=192.168.3.56
